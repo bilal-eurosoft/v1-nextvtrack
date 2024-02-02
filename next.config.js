1b0798e4d2f6d073c5768b65b4676dd5b9250119
bilal-eurosoft <bilal.hussain@eurosofttech.co.uk>
2024-02-02 15:09:12 +0500

    first commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {},
+  images: {
+    domains: ["vtracksolutions.s3.eu-west-2.amazonaws.com"],
+  },
+  compiler: {
+    styledComponents: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
