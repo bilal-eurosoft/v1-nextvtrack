99c974a26db6215c249176bb6a0c1eca1634780b
bilalmehfooz12 <bilalmehfooz253@gmail.com>
2024-03-11 17:04:54 +0500

    live_tracking_responsive_work

--- src/components/LiveTracking/LiveSidebar.tsx
+++ src/components/LiveTracking/LiveSidebar.tsx
@@ -135,7 +135,7 @@ const LiveSidebar = ({
     <div className="xl:col-span-1  lg:col-span-2  md:col-span-2 sm:col-span-4  col-span-4 main_sider_bar">
       <div className="grid grid-cols-12 bg-white py-3  lg:gap-0 gap-3 search_live_tracking">
         <div className="lg:col-span-7 w-full  md:col-span-5 sm:col-span-5 col-span-6 sticky top-0">
-          <div className="grid grid-cols-12">
+          <div className="grid grid-cols-12 vehicle_search_left">
             <div className="lg:col-span-1 md:col-span-1 sm:col-span-1">
               <svg
                 className="h-5 w-5 ms-1 mt-1 text-green "
@@ -162,12 +162,12 @@ const LiveSidebar = ({
             </div>
           </div>
         </div>
-        <div className="flex text-center lg:col-span-5  md:col-span-7  sm:col-span-5 col-span-5  w-full">
+        <div className="flex text-center lg:col-span-5  md:col-span-7  sm:col-span-5 col-span-5  w-full show_vehicle_left">
           <button
             className="text-center mx-auto text-md   w-full font-medium text-green mt-1"
             onClick={toggleLiveCars}
           >
-            Show({carData?.length}) Vehicles
+            Show ({carData?.length}) Vehicles
           </button>
         </div>
       </div>
