f6a1cb17b52a5411ae34757acd53b075c6dbf5ac
surakshaeurosoft <suraksha@eurosofttech.co.uk>
2024-04-29 19:46:12 +0500

    new

---
+++ src/utils/unixTimestamp.ts
@@ -0,0 +1,5 @@
+export function dateTimeToTimestamp(dateString: string, timeString: string) {
+    const combinedDateTimeString = dateString + 'T' + timeString;
+    const timestamp = new Date(combinedDateTimeString).getTime() / 1000;
+    return timestamp;
+  }
