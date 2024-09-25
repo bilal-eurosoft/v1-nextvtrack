ab9f128901e8fd2f496f6d577b7fde95356ed1fa
hammadayoub123 <m.hammad@eurosofttech.co.uk>
2024-09-25 14:28:57 +0500

    videotabwork

--- src/components/DualCam/view.tsx
+++ src/components/DualCam/view.tsx
@@ -309,7 +309,7 @@ export default function DualCam() {
         toast.dismiss(toastId);
         setToastId(null);
     }
-    else  if ( socketdata.filetype == ".mp4" && socketdata.progress > 1 && socketdata.progress < 100) {
+    else  if ( socketdata.filetype == ".h265" && socketdata.progress > 1 && socketdata.progress < 100) {
       
         if (!toastId) {
           const id = toast.loading("Video Downloading", {
